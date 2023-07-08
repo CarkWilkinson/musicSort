1ad75b3912cc3b496496ba63dabbee4454e4b7a2
Mark W <markwilkinson.141@gmail.com>
2023-07-08 17:23:42 -0400

    Initial launch of musicsort

---
+++ src/musicSort.nim
@@ -0,0 +1,40 @@
+# This is just an example to get you started. A typical binary package
+# uses this file as the main entry point of the application.
+import metatag/id3
+import os
+import std/sequtils
+import std/strformat
+
+const raw: string = "raw/"
+const sorted: string = "sorted/"
+const version:string = "DEVELOPMENT"
+
+proc moveMusicToRaw() =
+    createDir(raw)
+    createDir(sorted)
+    echo "wip"
+proc createSeqMusic(): seq[string] =
+    result = toSeq(walkFiles(raw&"*.mp3"))
+proc iterateSeq(list: seq[string]) = 
+#    for i in range(list.count()):
+#        list[i] = list[i].extractFilename()
+    for file in list:
+        echo file
+        var tag = readId3(file)
+        #for textFrame in tag.textFrames:
+        #    echo fmt"id: {textFrame.id}"
+        #    echo fmt"conent: {textFrame.content}"
+        let artist: string = tag.textFrames[1][1]
+        let album: string = tag.textFrames[2][1]
+        createDir(sorted&artist)
+        createDir(sorted&artist&"/"&album)
+        let endName: string = tag.textFrames[5][1]&" "&tag.textFrames[0][1]&".mp3"
+        copyFile(file, sorted&artist&"/"&album&"/"&endName)
+
+when isMainModule:
+    moveMusicToRaw()
+    var list: seq[string] = createSeqMusic()
+    echo list
+    iterateSeq(list)
+
+
