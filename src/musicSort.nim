28f30fc42b09851ce4c2c030d42078c9c139000c
Mark W <markwilkinson.141@gmail.com>
2023-07-08 21:41:38 -0400

    A / reads as \0 and causes issues with createDir, fixed

--- src/musicSort.nim
+++ src/musicSort.nim
@@ -12,35 +12,32 @@ const version:string = "DEVELOPMENT"
 proc moveMusicToRaw() =
     createDir(raw)
     createDir(sorted)
-    echo "wip"
 proc createSeqMusic(): seq[string] =
     result = toSeq(walkFiles(raw&"*.mp3"))
 proc iterateSeq(list: seq[string]) = 
-#    for i in range(list.count()):
-#        list[i] = list[i].extractFilename()
     for file in list:
-        echo file
         var tag = readId3(file)
-        #for textFrame in tag.textFrames:
-        #    echo fmt"id: {textFrame.id}"
-        #    echo fmt"conent: {textFrame.content}"
         var artist: string = tag.textFrames[1][1]
         var album: string = tag.textFrames[2][1]
         var name: string = tag.textFrames[0][1]
         var trackNum: string = tag.textFrames[5][1]
-        artist = artist.replace("/", "")
-        album = album.replace("/","")
-        name = name.replace("/", "")
+        # Whenever there is a / in the metadata it reads as a \0 which doesn't appear in echo and causes issues with createDir
+        artist = artist.replace("/", "-").replace(" ", "_").replace("\0", "-")
+        album = album.replace("/","").replace("\0", "-")
+        name = name.replace("/", "").replace("\0", "-")
         trackNum = trackNum.replace("/", "")
-        createDir(sorted&artist)
-        createDir(sorted&artist&"/"&album)
+
+        let artistDir: string = sorted&artist
+        let albumDir: string = artistDir&"/"&album
+
+        createDir(albumDir)
+
         let endName: string = trackNum&" "&name&".mp3"
-        copyFile(file, sorted&artist&"/"&album&"/"&endName)
+        copyFile(file, albumDir&"/"&endName)
 
 when isMainModule:
     moveMusicToRaw()
     var list: seq[string] = createSeqMusic()
-    echo list
     iterateSeq(list)
 
 
